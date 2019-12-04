3d131026406c2828d29ce2a7a9f7401628b5f40a
bquizza5 <blaine555@gmail.com>
2019-12-04 13:04:41 -0700

    day 1 mvp

--- src/adv.py
+++ src/adv.py
@@ -1,22 +1,23 @@
 from room import Room
+from player import Player
 
 # Declare all the rooms
 
 room = {
-    'outside':  Room("Outside Cave Entrance",
+    'outside':  Room("outside","Outside Cave Entrance",
                      "North of you, the cave mount beckons"),
 
-    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
+    'foyer':    Room("foyer","Foyer", """Dim light filters in from the south. Dusty
 passages run north and east."""),
 
-    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
+    'overlook': Room("overlook","Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
 the distance, but there is no way across the chasm."""),
 
-    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
+    'narrow':   Room("narrow","Narrow Passage", """The narrow passage bends here from west
 to north. The smell of gold permeates the air."""),
 
-    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
+    'treasure': Room("treasure", "Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
 earlier adventurers. The only exit is to the south."""),
 }
@@ -39,6 +40,8 @@ room['treasure'].s_to = room['narrow']
 
 # Make a new player object that is currently in the 'outside' room.
 
+player_1 = Player('player1', room['outside'])
+
 # Write a loop that:
 #
 # * Prints the current room name
@@ -49,3 +52,30 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
+running = True
+
+while running == True:
+    print('\nlocation: ',player_1.location.name)
+    print('Description: ',player_1.location.description)
+
+    action = input('\ntype action here: ').upper()
+
+    if action == 'N':
+        player_1.set_room(room[player_1.location.id].n_to)
+
+    elif action == 'E':
+        player_1.set_room(room[player_1.location.id].e_to)
+
+    elif action == 'S':
+        player_1.set_room(room[player_1.location.id].s_to)
+
+    elif action == 'W':
+        player_1.set_room(room[player_1.location.id].w_to)
+
+    elif action == 'Q':
+        running = False
+    else: 
+        print('\naction not valid\n')
+        print('N = move north\nE = move east\nS = move south\nW = move west\nQ = quit')
+
--- src/player.py
+++ src/player.py
@@ -1,2 +1,14 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+from room import Room
+
+class Player(object):
+    
+    def __init__(self, name, location):
+        self.name = name
+        self.location = location
+
+
+    def set_room(self, room):
+        self.location = room
